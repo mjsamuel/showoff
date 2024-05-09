aa54dc3a0de6afd30b6e6d21f501f319b22d6ca4
mjsamuel <matthew.samuel@outlook.com.au>
2024-05-09 21:58:58 +1000

    refactor game logic/setup

--- app.vue
+++ app.vue
@@ -6,48 +6,56 @@ useHead({
   bodyAttrs: { class: "bg-white dark:bg-gray-900" },
 });
 
+type Prompt = {
+  key?: string;
+  name: string;
+  description?: string | string[];
+  excluded?: string[];
+};
+
 type Data = {
-  categories: Category[];
-  modifiers: Modifier[];
+  categories: Record<string, Prompt>;
+  modifiers: Record<string, Prompt>;
 };
 
-type Category = {
-  name: string;
-  description?: string | string[];
-  excluded?: string[];
+type Card = {
+  category: Prompt;
+  modifier?: Prompt;
 };
 
-type Modifier = {
-  name: string;
-  description?: string | string[];
-  excluded?: string[];
-};
+const MODIFIER_PROBABILITY = 0.3;
 
-const DATA: Data = setupData(); // backup of data
-let categories: Category[] = [];
-let modifiers: Modifier[] = [];
+const data = {
+  categories: preparePrompts(jsonData.categories),
+  modifiers: preparePrompts(jsonData.modifiers),
+};
 let playWithModifiers = true;
 
 const showRules = ref(false);
-const currCategory = ref<Cateogry | null>(null);
-const currModifier = ref<Modifier | null>(null);
+const cards = ref<Card[]>([]);
 
-function setupData() {
-  let data: Data = jsonData;
-
-  data.categories.forEach((category) => {
-    if (category.description instanceof Array) {
-      category.description = category.description.join("");
+function preparePrompts(prompts: Record<string, Prompt>) {
+  return Object.entries(prompts).map(([key, prompt]) => {
+    if (prompt.description instanceof Array) {
+      prompt.description = prompt.description.join("");
     }
+    return { key, ...prompt };
   });
+}
 
-  data.modifiers.forEach((modifier) => {
-    if (modifier.description instanceof Array) {
-      modifier.description = modifier.description.join("");
-    }
-  });
+function getPrompt(type: "categories" | "modifiers", excluded?: string[]) {
+  let prompts = data[type];
+  if (prompts.length === 0) {
+    prompts = preparePrompts(jsonData[type] as Record<string, Prompt>);
+  }
 
-  return data;
+  const index = randomNumberInRange(0, prompts.length - 1);
+  let prompt: Prompt | null = null;
+  while (!prompt) {
+    prompt = prompts[index];
+  }
+  prompts.splice(index, 1);
+  return prompt;
 }
 
 function startGame(useModifiers: boolean) {
@@ -56,50 +64,22 @@ function startGame(useModifiers: boolean) {
 }
 
 function nextTurn() {
-  currModifier.value = null;
-  currCategory.value = getCategory();
-  if (playWithModifiers && Math.random() < 1) {
-    currModifier.value = getModifier();
+  const category = getPrompt("categories");
+  let modifier;
+  if (playWithModifiers && Math.random() < MODIFIER_PROBABILITY) {
+    modifier = getPrompt("modifiers");
   }
+  cards.value.unshift({ category, modifier });
 }
 
-function getCategory(): Category {
-  let category = categories.pop();
-  if (category === undefined) {
-    categories = [...DATA.categories];
-    shuffleArray(categories);
-    category = categories.pop();
-  }
-  return category!;
-}
-
-function getModifier(): Modifier {
-  let modifier = modifiers.pop();
-  if (modifier === undefined) {
-    modifiers = [...DATA.modifiers];
-    shuffleArray(modifiers);
-    modifier = modifiers.pop();
-  }
-  return modifier!;
-}
-
-function shuffleArray(array: any[]) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+function randomNumberInRange(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 </script>
 
 <template>
-  <Transition
-    enter-active-class="transition-opacity duration-300 ease-out"
-    leave-active-class="transition-opacity duration-200"
-    enter-from-class="opacity-0 "
-    leave-to-class="opacity-0"
-  >
+  <Transition enter-active-class="transition-opacity duration-300 ease-out"
+    leave-active-class="transition-opacity duration-200" enter-from-class="opacity-0 " leave-to-class="opacity-0">
     <RulesModal v-if="showRules" @close-modal="showRules = false"></RulesModal>
   </Transition>
 
@@ -107,23 +87,14 @@ function shuffleArray(array: any[]) {
     <div class="flex h-[calc(100dvh)] flex-col sm:w-[80%] lg:w-[50%]">
       <header class="m-5 flex">
         <h1 class="grow text-4xl font-extrabold dark:text-white">Showoff</h1>
-        <HamburgerButton
-          class="h-9 w-9 text-gray-800 dark:text-white"
-          @click="showRules = true"
-        >
+        <HamburgerButton class="h-9 w-9 text-gray-800 dark:text-white" @click="showRules = true">
         </HamburgerButton>
       </header>
 
       <div class="flex grow flex-col">
-        <SolidButton class="mx-auto mb-6 w-28" @click="nextTurn()"
-          >Next Turn</SolidButton
-        >
+        <SolidButton class="mx-auto mb-6 w-28" @click="nextTurn()">Next Turn</SolidButton>
         <div class="relative z-0 m-auto h-full w-96">
-          <Card
-            class="z-10"
-            :category="currCategory"
-            :modifier="currModifier"
-          ></Card>
+          <Card v-for="(card, index) in cards" class="z-10" :category="card.category" :modifier="card.modifier"></Card>
         </div>
       </div>
     </div>
--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,8 @@
 <template>
-  <Transition
-    enter-active-class="transition-all duration-300 ease-out"
-    enter-from-class="translate-y-full"
-    @after-enter="showModifier = true"
-  >
-    <div
-      v-if="category"
-      class="flex h-full w-full select-none flex-col rounded-t-xl border-black bg-gray-100 shadow-xl"
-    >
+  <Transition enter-active-class="transition-all duration-300 ease-out" enter-from-class="translate-y-full"
+    @after-enter="showModifier = true">
+    <div v-if="category"
+      class="flex h-full w-full select-none flex-col rounded-t-xl border-black bg-gray-100 shadow-xl">
       <!-- category card -->
       <div class="p-5">
         <p class="text-3xl font-semibold text-black">{{ category.name }}</p>
@@ -16,14 +11,9 @@
         </p>
       </div>
       <!-- modifier card -->
-      <Transition
-        enter-active-class="transition-all delay-75 duration-300 ease-out"
-        enter-from-class="translate-y-full"
-      >
-        <div
-          v-if="showModifier && modifier"
-          class="flex-grow rounded-t-xl border-black bg-black p-5 shadow-xl"
-        >
+      <Transition enter-active-class="transition-all delay-75 duration-300 ease-out"
+        enter-from-class="translate-y-full">
+        <div v-if="showModifier && modifier" class="flex-grow rounded-t-xl border-black bg-black p-5 shadow-xl">
           <p class="text-3xl font-semibold text-white">{{ modifier.name }}</p>
           <p class="text-lg font-semibold text-white">
             {{ modifier.description }}
