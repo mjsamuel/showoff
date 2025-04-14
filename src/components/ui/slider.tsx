3ff0974870aa95cedcc41f873a936554b339e58e
mjsamuel <hello@mjsamuel.com>
2025-04-14 21:04:28 +1000

    increase slider touch target

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -31,7 +31,7 @@ function Slider({
       min={min}
       max={max}
       className={cn(
-        "relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col hover:cursor-pointer data-[disabled]:hover:cursor-not-allowed",
+        "relative flex w-full touch-none items-center select-none hover:cursor-pointer data-[disabled]:opacity-50 data-[disabled]:hover:cursor-not-allowed data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
         className,
       )}
       {...props}
@@ -53,7 +53,7 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+          className="border-primary bg-background ring-ring/50 block size-5 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 md:size-4"
         />
       ))}
     </SliderPrimitive.Root>
